c1ca3e0c67e8b0ffebe3dc073bc6e122213f12ed
Robert Csikos <143592673+csro23ei@users.noreply.github.com>
2024-06-05 10:42:31 +0200

    Add files via upload

---
+++ src/main/java/com/AdressBok/AdresBokApplication.java
@@ -0,0 +1,13 @@
+package com.AdressBok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdresBokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdresBokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AdressBok/AdresBokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AdressBok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdresBokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
